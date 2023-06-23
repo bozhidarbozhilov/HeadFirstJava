94dcc1d49289cb370a8c2fd20acf9b4c24c108b1
Bozhidar Bozhilov <bozhidarbozhilov@gmail.com>
2023-06-23 15:53:47 +0300

    add functionality to chat app

--- Code/src/simplechatapp/SimpleChatClientA.java
+++ Code/src/simplechatapp/SimpleChatClientA.java
@@ -2,29 +2,40 @@ package simplechatapp;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.nio.channels.Channels;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static java.nio.charset.StandardCharsets.*;
 
 public class SimpleChatClientA {
+    private JTextArea incoming;
     private JTextField outgoing;
+    private BufferedReader reader;
+
     private PrintWriter writer;
 
     public void go(){
         setUpNetworking();
+        JScrollPane scroller = createScrollableTextArea();
         outgoing = new JTextField(20);
         JButton sendBtn = new JButton("Send");
 
         sendBtn.addActionListener(e->sendMessage());
 
         JPanel mainPanel = new JPanel();
+        mainPanel.add(scroller);
         mainPanel.add(outgoing);
         mainPanel.add(sendBtn);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(new IncomingReader());
 
         JFrame frame = new JFrame("Ludicrously Simple Chat Client");
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
@@ -32,6 +43,17 @@ public class SimpleChatClientA {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+    }
+
+    private JScrollPane createScrollableTextArea() {
+        incoming = new JTextArea(15,30);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane scroller = new JScrollPane(incoming);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        return scroller;
     }
 
     private void sendMessage() {
@@ -44,12 +66,28 @@ public class SimpleChatClientA {
     private void setUpNetworking() {
         try{
             InetSocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 5050);
-
             SocketChannel socketChannel = SocketChannel.open(serverAddress);
+
             writer = new PrintWriter(Channels.newWriter(socketChannel, UTF_8));
+            reader = new BufferedReader(Channels.newReader(socketChannel, UTF_8));
             System.out.println("Networking established.");
         } catch(IOException ex){
             ex.printStackTrace();
+        }
+    }
+
+    private class IncomingReader implements Runnable {
+        @Override
+        public void run() {
+            String message;
+            try{
+                while((message = reader.readLine()) != null){
+                    System.out.println("read " + message);
+                    incoming.append(message + "\n");
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
         }
     }
 
--- Code/src/threads/ExecutorTestDrive.java
+++ Code/src/threads/ExecutorTestDrive.java
@@ -1,13 +1,37 @@
 package threads;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorTestDrive {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.execute(()-> System.out.println("Top o' stack"));
+        CountDownLatch latch = new CountDownLatch(1);
+        //executor.execute(ExecutorTestDrive::sleepThenPrint);
+        executor.execute(()->waitForLatchThenPrint(latch));
         System.out.println("Back in main");
+        latch.countDown();
         executor.shutdown();
+    }
+
+    private static void waitForLatchThenPrint(CountDownLatch latch) {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("top o' stack with latch");
+    }
+
+    //A try to order threads with sleep() during learning process
+    private static void sleepThenPrint(){
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("top o' the stack");
     }
 }
